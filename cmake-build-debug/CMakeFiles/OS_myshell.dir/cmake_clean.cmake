ef71deb591cdcb122d092a0ace5e6ba6035fac34
dfghjm <popov@ucu.edu.ua>
2018-03-18 21:33:20 +0200

    Boost clion support

---
+++ cmake-build-debug/CMakeFiles/OS_myshell.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_myshell.dir/shell.cpp.o"
+  "OS_myshell.pdb"
+  "OS_myshell"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS_myshell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ shell.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Rostyk Popov on 3/18/18.
+//
+#include <iostream>
+using namespace std;
+int main(){
+    return 0;
+}
