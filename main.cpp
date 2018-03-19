0f68bb57d8320e0262ba64f41a466f6faa0da623
dfghjm <popov@ucu.edu.ua>
2018-03-19 17:28:59 +0200

    args parse

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Rostyk Popov on 3/19/18.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string inte = "dvadcat' dva";
+    int one = stoi(inte);
+    cout<<one;
+}
--- shell.cpp
+++ shell.cpp
@@ -1,11 +1,14 @@
+
 #include <iostream>
 #include <stdio.h>
 #include <unistd.h>
 #include <sstream>
 #include <vector>
 #include <string.h>
+#include <boost/algorithm/string.hpp>
 
 using namespace std;
+using namespace boost;
 
 void cwd() {
     char dir[100];
@@ -15,42 +18,38 @@ void cwd() {
 void cd(string path) {
     chdir(path.c_str());
 }
-vector<string> parse_args(string str) {
-    vector<string> vector1;
-    istringstream iss(str);
-    while(iss) {
-        string subs;
-        iss>>subs;
-        vector1.push_back(subs);
-    }
-    vector1.pop_back();
-    return vector1;
-}
 
 void execute(vector<string> args) {
-    
+
 }
 
 vector<string> read_args(string cmd) {
     vector<string> args;
-    
-    if (cmd == "exit") {
-        exit(0);
-    }
     args.push_back(cmd);
     return args;
 }
 
+void myexit(string fisrt, string second){
+    int exit_num;
+    if(first == "-h" && first == "--help"){
+        exit_num = stoi(second);
+    }
+    else{
+        exit_num = stoi(fisrt);
+    }
+    return 
+}
+
+
 int main() {
     string cmd;
     vector<string> args;
-    
-    while(cin >> cmd) {
-        args = read_args(cmd);
-        for (auto &a : args) {
-            cout << a << endl;
+    while(true){
+        getline(cin, cmd);
+        split(args, cmd, is_any_of("\t "), token_compress_on);
+        if (args[0] == "exit"){
+            myexit(args[1], args[2]);
         }
     }
-    
     return 0;
 }
