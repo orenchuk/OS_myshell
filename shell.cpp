5f76978f542613eb7c92c617287e2a285e586f3b
Yevhenii Orenchuk <orenchuk98@gmail.com>
2018-03-19 19:26:14 +0200

    added mpwd and cd

--- shell.cpp
+++ shell.cpp
@@ -1,80 +1,82 @@
-
 #include <iostream>
 #include <stdio.h>
 #include <unistd.h>
 #include <sstream>
 #include <vector>
-#include <string.h>
+#include <string>
 #include <boost/algorithm/string.hpp>
 #include <boost/filesystem/operations.hpp>
 
 using namespace std;
 using namespace boost;
 
-void mpwd() {
-    auto path = boost::filesystem::current_path().string();
-    cout<<path;
+void mypwd() {
+    auto cwd = filesystem::current_path().string();
+    cout << cwd << endl;
 }
-void mcd(string path) {
+
+void mycd(string path) {
     chdir(path.c_str());
 }
 
-void execute(vector<string> args) {
-
-}
-
-vector<string> read_args(string cmd) {
-    vector<string> args;
-    args.push_back(cmd);
-    return args;
-}
-
-int myexit(vector<string> args){
+void myexit(vector<string> args) {
     int exit_num = 0;
     string help = "help was made";
-    if(args.size() == 3){
-        if(args[2] == "-h" && args[2] == "--help"){
-            cout<<help;
-            string num = args[2];
-            exit_num = stoi(num);
-        }
-        else{
-            string num = args[1];
-            exit_num = stoi(num);
-            cout<<help;
-        }
-    }
-    else if (args.size() == 2){
-        if (args[1] == "-h" && args[1] == "--help"){
-            cout<<help;
-        }
-        else{
+    if(args.size() == 2) {
+        if(args[2] == "-h" && args[1] == "--help") {
+            cout<<help<<endl;
             string num = args[1];
             exit_num = stoi(num);
         }
+        else {
+            string num = args[0];
+            exit_num = stoi(num);
+            cout<<help<<endl;
+        }
     }
-    else{
-        return exit_num;
+    else if (args.size() == 1) {
+        if (args[0] == "-h" && args[0] == "--help") {
+            cout<<help<<endl;
+        }
+        else {
+            string num = args[0];
+            exit_num = stoi(num);
+        }
     }
-    return exit_num;
+    else {
+        exit(exit_num);
+    }
+    exit(exit_num);
 }
 
+void execute(vector<string> args) {
+    string cmd = args[0];
+    //    cout << "cmd: " << cmd << endl;
+    args.erase(args.begin());
+    //    for (auto &a : args) {
+    //        cout << a << endl;
+    //    }
+    if (cmd == "merrno") {
+        
+    } else if (cmd == "mpwd") {
+        mypwd();
+    } else if (cmd == "mcd") {
+        mycd("../");
+    } else if (cmd == "mexit") {
+        myexit(args);
+    }
+}
 
 int main() {
     string cmd;
     vector<string> args;
-    while(true){
-        mpwd();
-        cout<<" $ ";
+    while(true) {
         getline(cin, cmd);
-        split(args, cmd, is_any_of("\t "), token_compress_on);
-        if (args[0] == "mexit"){
-            return myexit(args);
-            }
-        if(args[0] == "mpwd"){
-            mpwd();
-        }
-
-        }
+        split(args, cmd, is_any_of(" "), token_compress_on);
+        execute(args);
+        //        for (auto &a : args) {
+        //            cout << "test: " << a << endl;
+        //        }
+    }
     return 0;
 }
