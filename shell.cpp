b7b473971a4cb9566b16b191f6a863631fd30219
Yevhenii Orenchuk <orenchuk98@gmail.com>
2018-06-07 21:11:45 +0300

    myshell version 2.0, done

--- shell.cpp
+++ shell.cpp
@@ -9,6 +9,10 @@
 #include <boost/filesystem.hpp>
 #include <boost/tokenizer.hpp>
 #include <sys/wait.h>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <boost/algorithm/string.hpp>
 
 using std::cout;
 using std::cin;
@@ -20,7 +24,8 @@ boost::filesystem::path pwd() {
     return boost::filesystem::current_path();
 }
 
-int mypwd(const vector<string>& args) {
+int mypwd(vector<string>& args) {
+    args.erase(args.begin());
     int erno = 0;
     string help = "[-h|--help] - returns current directory path";
     string error = "Error: there are no such options, use -h or --help to get more info";
@@ -32,13 +37,14 @@ int mypwd(const vector<string>& args) {
         cout << help << endl;
     } else {
         erno = -1;
-        cout << error << endl;
+        std::cerr << error << endl;
     }
     
     return erno;
 }
 
-int mycd(const vector<string>& args) {
+int mycd(vector<string>& args) {
+    args.erase(args.begin());
     string help = "[-h|--help] - change directory to <path>";
     if (args.size() == 1 && chdir(args[0].c_str()) == 0) {
         return 0;
@@ -46,59 +52,189 @@ int mycd(const vector<string>& args) {
         cout<< help <<endl;
         return chdir(args[0].c_str());
     }
-    else if(args.size() == 1 && (args[0] == "-h" || args[0] == "--help")){
+    else if(args.size() == 1 && (args[0] == "-h" || args[0] == "--help")) {
         cout << help << endl;
     }
     return 1;
 }
 
-void myexit(const vector<string>& args) {
+void myexit(vector<string>& args) {
+    args.erase(args.begin());
     int exit_num = 0;
     string help = "help was made";
     if(args.size() == 2) {
         if(args[2] == "-h" && args[1] == "--help") {
-            cout<<help<<endl;
+            cout << help << endl;
             string num = args[1];
             exit_num = stoi(num);
-        }
-        else {
+        } else {
             string num = args[0];
             exit_num = stoi(num);
-            cout<<help<<endl;
+            cout << help << endl;
         }
-    }
-    else if (args.size() == 1) {
+    } else if (args.size() == 1) {
         if (args[0] == "-h" && args[0] == "--help") {
-            cout<<help<<endl;
-        }
-        else {
+            cout << help << endl;
+        } else {
             string num = args[0];
             exit_num = stoi(num);
         }
-    }
-    else {
+    } else {
         exit(exit_num);
     }
     exit(exit_num);
 }
 
-void execute(vector<string>& args) {
+void myforkexec(const vector<string>& args) {
+    
+    pid_t pid = fork();
+    if (pid == -1)
+    {
+        std::cerr << "Failed to fork()" << std::endl;
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) {
+        int status;
+        waitpid(pid, &status, 0);
+    } else {
+        auto path_ptr = getenv("PATH");
+        string path_var;
+        if (path_ptr != nullptr) {
+            path_var = path_ptr;
+        } else {
+            std::cerr << "Error no such argumet(s), try \"help\" for more info" << endl;
+        }
+        path_var += ":.";
+        setenv("PATH", path_var.c_str(), 1);
+        vector<const char*> arg_for_c;
+        for (auto s: args)
+            arg_for_c.push_back(s.c_str());
+        arg_for_c.push_back(nullptr);
+        execvp(args.front().c_str(), const_cast<char* const*>(arg_for_c.data()));
+        std::cerr << "Error: there is no such argument(s)" << endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+vector<string> replace(vector<string>& args, std::map<string, string>& vars) {
+    vector<string> new_args;
+    for (auto& arg : args) {
+        if (arg[0] == '$') {
+            arg.erase(arg.begin());
+            if (vars.count(arg) > 0) {
+                new_args.push_back(vars[arg]);
+            } else if (getenv(arg.c_str()) != NULL) {
+                new_args.push_back(getenv(arg.c_str()));
+            } else {
+                std::cerr << "Error: there is no such variable" << endl;
+            }
+        } else {
+            new_args.push_back(arg);
+        }
+    }
+    return new_args;
+}
+
+void setvars(std::map<string, string>& vars, const string& line) {
+    vector<string> res;
+    boost::split(res, line, boost::is_any_of("="));
+    vars[res.front()] = res.back();
+    cout << res.front() << " is localy set to " << vars[res.front()] << endl;
+}
+
+void mysetenv(string line, std::map<string, string>& vars) {
+    if (boost::contains(line, "=")) {
+        vector<string> res;
+        boost::split(res, line, boost::is_any_of("="));
+        setenv(res.front().c_str(), res.back().c_str(), 1);
+        cout << res.front() << " is exportly set to " << getenv(res.front().c_str()) << endl;
+    } else if (boost::contains(line, "$")) {
+        line.erase(line.begin());
+        if (vars.count(line) > 0) {
+            setenv(line.c_str(), vars[line].c_str(), 0);
+        }
+        cout << "variable " << line << " is exportly set to " << getenv(line.c_str()) << endl;
+    }
+}
+
+void execute_script(std::map<string, string>& vars, const vector<string>& args) {
+    for (auto& a : args) {
+        std::ifstream input(a);
+        if (!input.is_open()) {
+            std::cerr << "Error: there is no such file" << endl;
+        }
+
+        string line;
+        while (std::getline(input, line)) {
+            vector<string> lines;
+            std::istringstream sstream(line);
+            string word;
+            while (sstream >> word) {
+                if (word[0] == '#') {
+                    break;
+                } else if (word[0] == '$') {
+                    word.erase(word.begin());
+                    if (vars.count(word) > 0) {
+                        auto variable = vars[word];
+                        lines.push_back(variable);
+                    } else if (getenv(word.c_str()) != NULL) {
+                        lines.push_back(getenv(word.c_str()));
+                    } else {
+                        std::cerr << "Error: there is no such var" << endl;
+                    }
+                } else if (word == "mexport") {
+                    sstream >> word;
+                    if (boost::contains(word, "=")) {
+                        setvars(vars, word);
+                    }
+                    mysetenv(word, vars);
+                    continue;
+                } else if (word == "mecho") {
+                    lines.push_back("echo");
+                } else if (boost::contains(word, "=")) {
+                    setvars(vars, word);
+                    continue;
+                } else {
+                    lines.push_back(word);
+                }
+            }
+            myforkexec(lines);
+        }
+    }
+    
+}
+
+void execute(std::map<string, string>& vars, vector<string>& args) {
     int err_code = 0;
     if (args.empty()) {
         exit(-2);
     }
     string cmd = args[0];
-    args.erase(args.begin());
     
     if (cmd == "merrno") {
         cout << err_code << endl;
     } else if (cmd == "mpwd") {
-        err_code = mypwd(args);
+        auto new_args = replace(args, vars);
+        err_code = mypwd(new_args);
     } else if (cmd == "mcd") {
-        err_code = mycd(args);
+        auto new_args = replace(args, vars);
+        err_code = mycd(new_args);
     } else if (cmd == "mexit") {
-        myexit(args);
-        
+        auto new_args = replace(args, vars);
+        myexit(new_args);
+    } else if (cmd == "mecho") {
+        args[0] = "echo";
+        auto new_args = replace(args, vars);
+        myforkexec(new_args);
+    } else if (cmd == "mexport") {
+        args.erase(args.begin());
+        for (auto& a : args) {
+            if (boost::contains(a, "=")) {
+                setvars(vars, a);
+            }
+            mysetenv(a, vars);
+        }
+    } else if (boost::contains(cmd, "=")) {
+        setvars(vars, cmd);
     } else if (cmd == "help") {
         cout << "Program MyShell. version 1.0 beta release\n" << endl;
         cout << "merrno [-h|--help] \t returns exit status of the command" << endl;
@@ -106,39 +242,33 @@ void execute(vector<string>& args) {
         cout << "mcd <path> [-h|--help] \t change dir to <path>" << endl;
         cout << "mexit [exit code] [-h|--help] \t exit myshell\n" << endl;
     } else {
-        pid_t pid = fork();
-        if (pid == -1)
-        {
-            std::cerr << "Failed to fork()" << std::endl;
-            exit(EXIT_FAILURE);
-        } else if (pid > 0) {
-            int status;
-            waitpid(pid, &status, 0);
+        if (cmd[0] == '.') {
+            args.front().erase(args.front().begin());
+            execute_script(vars, args);
         } else {
-            auto path_ptr = getenv("PATH");
-            string path_var;
-            if (path_ptr != nullptr) {
-                path_var = path_ptr;
-            } else {
-                cout << "Error no such argumet(s), try \"help\" for more info" << endl;
-            }
-            path_var += ":.";
-            setenv("PATH", path_var.c_str(), 1);
-            vector<const char*> arg_for_c;
-            arg_for_c.push_back(cmd.c_str());
-            for (auto s: args)
-                arg_for_c.push_back(s.c_str());
-            arg_for_c.push_back(nullptr);
-            execvp(cmd.c_str(), const_cast<char* const*>(arg_for_c.data()));
-            exit(EXIT_FAILURE);
+            myforkexec(replace(args, vars));
         }
     }
 }
 
-int main() {
+int main(int argc, const char * argv[]) {
+    
+    std::map<string, string> variables;
+    
     string separator1("");      //dont let quoted arguments escape themselves
     string separator2(" ");     //split on spaces
     string separator3("\"\'");  //let it have quoted arguments
+    
+    if (argc > 1) {
+        vector<string> vect;
+        for (size_t i = 1; i < argc; ++i) {
+            vect.push_back(argv[i]);
+        }
+        
+        execute_script(variables, vect);
+        
+        exit(EXIT_SUCCESS);
+    }
     
     while(true) {
         string cmd;
@@ -154,7 +284,8 @@ int main() {
             args.push_back(t);
         }
         
-        execute(args);
+        execute(variables, args);
     }
+    
     return 0;
 }
